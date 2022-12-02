d1fa346777c309f14afeefcf7b8bf56cb1421521
coderAsh1 <ashutoshsasmal1@gmail.com>
2022-12-02 14:15:55 +0530

    mobile-nav done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import "./navbar.scss";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaMoon, FaAngleDown } from "react-icons/fa";
-import { SlMenu } from "react-icons/sl";
+import { SlClose, SlMenu } from "react-icons/sl";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  window.addEventListener("click", () => setIsMobileView(false));
+
   return (
     <section className="navbar">
       <div className="logo">
@@ -41,7 +46,40 @@ const Navbar = () => {
           <AiOutlineShoppingCart />
           Buy now
         </button>
-        <SlMenu className="menu" />
+        <SlMenu
+          className="menu"
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsMobileView(!isMobileView);
+          }}
+          style={{ display: isMobileView && "none" }}
+        />
+        <SlClose
+          className="menu"
+          onClick={() => setIsMobileView(!isMobileView)}
+          style={{ display: !isMobileView && "none" }}
+        />
+      </div>
+      <div
+        className="mobile-menu"
+        style={{ width: isMobileView && "200px" }}
+        // onClick={(e) => e.stopPropagation()}
+      >
+        <div className="mobile-nav">
+          <ul>
+            <li>
+              Landings <FaAngleDown style={{ fontSize: "12px" }} />
+            </li>
+            <li>
+              Pages <FaAngleDown style={{ fontSize: "12px" }} />
+            </li>
+            <li>
+              Portfolio <FaAngleDown style={{ fontSize: "12px" }} />
+            </li>
+            <li>Portfolio</li>
+            <li>Docs</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
